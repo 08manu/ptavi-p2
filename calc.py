9be6e2ef24c081b3c96244428e43471178a94c66
Manuel Antonio Viso Troyano <maviso@gamma30.aulas.gsyc.urjc.es>
2016-09-19 10:36:29 +0200

    he añadido multiplicar y dividir

--- calc.py
+++ calc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import sys
+import sys 
 
 
 def plus(op1, op2):
@@ -13,10 +13,20 @@ def minus(op1, op2):
     """ Function to substract the operands """
     return op1 - op2
 
+def mult(op1, op2):
+	return op1 * op2
+
+def div(op1, op2):
+	return op1 / op2
+
+
+
 if __name__ == "__main__":
+	#print(sys.argv)
+	#sys.exit()
     try:
-        operando1 = int(sys.argv[1])
-        operando2 = int(sys.argv[3])
+        operando1 = int(sys.argv[1]) #como tiene corchete puede ser una lista o un diccionario
+        operando2 = int(sys.argv[3]) #sys,arg -->argumentos que me va a pasar el sistema(3 suma 2)
     except ValueError:
         sys.exit("Error: Non numerical parameters")
 
@@ -24,6 +34,11 @@ if __name__ == "__main__":
         result = plus(operando1, operando2)
     elif sys.argv[2] == "resta":
         result = minus(operando1, operando2)
+	elif sys.argv[2] == "multiplicar":
+		result = mult(operando1, operando2)
+	elif sys.argv[2] == "dividir":
+		result = div(operando1, operando2)
+
     else:
         sys.exit('Operación sólo puede ser sumar o restar.')
 
