2a30be7aa31971e3aa9beeb2b5c2fc71d6637878
Manuel Antonio Viso Troyano <maviso@gamma28.aulas.gsyc.urjc.es>
2016-09-26 15:19:47 +0200

    comprobacion calcplus

--- calcplus.py
+++ calcplus.py
@@ -1,36 +1,37 @@
 #!/usr/bin/python3
-#-*- coding: utf-8-*-
+# -*- coding: utf-8 -*-
 
 import sys
 import calcoohija
+
 
 fich = open(sys.argv[1] , 'r')
 lineas = fich.readlines()
 
 
-
 calculadora = calcoohija.CalculadoraHija()
+
 
 for linea in lineas:
 	element = linea[:-1].split(',')
 
-		
+
 	if element[0] == "suma":
-		resultado = calculadora.suma(int(element[1]), int(element[2]))		     
+		resultado = calculadora.suma(int(element[1]), int(element[2]))
 		for operacion in element[3:]:
-		    resultado = calculadora.suma(resultado, int(operacion)) 
+			resultado = calculadora.suma(resultado, int(operacion))
 	elif element[0] == "resta":
-		resultado = calculadora.resta(int(element[1]), int(element[2]))		     
+		resultado = calculadora.resta(int(element[1]), int(element[2]))
 		for operacion in element[3:]:
-		    resultado = calculadora.resta(resultado, int(operacion))  
+			resultado = calculadora.resta(resultado, int(operacion))
 	elif element[0] == "multiplica":
-		resultado = calculadora.mult(int(element[1]), int(element[2]))		     
+			resultado = calculadora.mult(int(element[1]), int(element[2])
 		for operacion in element[3:]:
-		    resultado = calculadora.mult(resultado, int(operacion)) 
+			resultado = calculadora.mult(resultado, int(operacion))
 	elif element[0] == "divide":
-		resultado = calculadora.div(int(element[1]), int(element[2]))		     
+			resultado = calculadora.div(int(element[1]), int(element[2]))
 		for operacion in element[3:]:
-		   resultado = calculadora.div(resultado, int(operacion))    
+			resultado = calculadora.div(resultado, int(operacion))
 	print(resultado)
 
 		
